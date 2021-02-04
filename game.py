d9e9b195b7aefd486a875b38030001d205aba6b4
Justin <67277072+SunghuJustinKim@users.noreply.github.com>
2021-02-03 22:53:47 -0600

    Add files via upload

--- game.py
+++ game.py
@@ -68,7 +68,7 @@ class Ship:
             laser.move(vel)
             if laser.off_screen(HEIGHT):
                 self.lasers.remove(laser)
-            elif laser.collision():
+            elif laser.collision(obj):
                 obj.health -= 10
                 self.lasers.remove(laser)
 
@@ -112,6 +112,14 @@ class Player(Ship):
                         objs.remove(obj)
                         self.lasers.remove(laser)
 
+    def draw(self, window):
+        super().draw(window)
+        self.healthbar(window)
+    
+    def healthbar(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 10))
+        pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width()* (self.health/self.max_health), 10))
+
 class Enemy(Ship):
     COLOR_MAP = {
             "red": (RED_SPACE_SHIP, RED_LASER),
@@ -127,6 +135,12 @@ class Enemy(Ship):
 
     def move(self, vel):
         self.y += vel
+    
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x-20, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
 
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
@@ -216,13 +230,35 @@ def main():
         for enemy in enemies[:]:
             enemy.move(enemy_vel) 
             enemy.move_lasers(laser_vel, player)
-            if enemy.y + enemy.get_height() > HEIGHT:
+
+            if random.randrange(0, 2*60)== 1:
+                enemy.shoot()
+            
+            if collide(enemy, player):
+                player.health -= 10
+                enemies.remove(enemy)
+
+            elif enemy.y + enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
+            
+            
         
         player.move_lasers(-laser_vel, enemies)
 
+def main_menu():
+    title_font = pygame.font.SysFont("comicsans", 80)
+    run = True
+    while run:
+        WIN.blit(BG, (0,0))
+        title_label = title_font.render("Mouse 1 to shoot", 1, (0,255,0))
+        WIN.blit(title_label,(WIDTH/2 - title_label.get_width()/2, 350))
 
-        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+    pygame.quit()       
 
-main()
+main_menu()
